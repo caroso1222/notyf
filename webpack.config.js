5cb95a82215c1abf38770b6dfde7ca093504199f
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2018-05-27 01:39:25 -0500

    chore: change build system to umd

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'Notyf',
-    libraryTarget: 'window',
+    libraryTarget: 'umd',
     libraryExport: 'default',
     umdNamedDefine: true,
   }
