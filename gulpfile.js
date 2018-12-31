ebba4f5e341fcbbe0e216f389768d87cca40e59e
Alexander Repeta <alexander.repeta@gmail.com>
2018-12-31 14:58:37 +0200

    Added zindex property to cssnano options in gulpfile.js so it would not reset z-index to 1

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('style', function () {
     .pipe(autoprefixer({
     browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
     }))
-    .pipe(cssnano())
+    .pipe(cssnano({ zindex: false }))
     .pipe(rename('notyf.min.css'))
     .pipe(gulp.dest('./dist'))
 })
